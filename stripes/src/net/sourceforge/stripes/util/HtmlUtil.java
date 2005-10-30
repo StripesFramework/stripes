1821abf630f6ea32a104449d999bdaf935fcacbb
Tim Fennell <tfenne@tfenne.com>
2005-10-30 17:26:43 +0000

    Added a pair of methods to HTML util to conjoin and split apart multiple values for placement in form fields.

--- stripes/src/net/sourceforge/stripes/util/HtmlUtil.java
+++ stripes/src/net/sourceforge/stripes/util/HtmlUtil.java
@@ -15,12 +15,19 @@
  */
 package net.sourceforge.stripes.util;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
 /**
  * Provides simple utility methods for dealing with HTML.
  *
  * @author Tim Fennell
  */
 public class HtmlUtil {
+    private static final String FIELD_DELIMITER_STRING = "||";
+    private static final Pattern FIELD_DELIMITER_PATTERN = Pattern.compile("\\|\\|");
 
     /**
      * Replaces special HTML characters from the set {@literal [<, >, ", ', &]} with their HTML
@@ -46,5 +53,47 @@ public class HtmlUtil {
         }
 
         return builder.toString();
+    }
+
+    /**
+     * One of a pair of methods (the other is splitValues) that is used to combine several
+     * un-encoded values into a single delimited, encoded value for placement into a
+     * hidden field.
+     *
+     * @param values One or more values which are to be combined
+     * @return a single HTML-encoded String that contains all the values in such a way that
+     *         they can be converted back into a Collection of Strings with splitValues().
+     */
+    public static String combineValues(Collection<String> values) {
+        if (values == null || values.size() == 0) {
+            return "";
+        }
+        else {
+            StringBuilder builder = new StringBuilder(values.size() * 30);
+            for (String value : values) {
+                builder.append(value).append(FIELD_DELIMITER_STRING);
+            }
+            
+            return encode(builder.toString());
+        }
+    }
+
+    /**
+     * Takes in a String produced by combineValues and returns a Collection of values that
+     * contains the same values as originally supplied to combineValues.  Note that the order
+     * or items in the collection (and indeed the type of Collection used) are not guaranteed
+     * to be the same.
+     *
+     * @param value a String value produced by
+     * @return a Collection of zero or more Strings
+     */
+    public static Collection<String> splitValues(String value) {
+        if (value == null || value.length() == 0) {
+            return Collections.emptyList();
+        }
+        else {
+            String[] splits = FIELD_DELIMITER_PATTERN.split(value);
+            return Arrays.asList(splits);
+        }
     }
 }
