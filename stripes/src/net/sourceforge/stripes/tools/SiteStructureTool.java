2001e00d0ed91b0a866d86b19f3b407633a76707
Tim Fennell <tfenne@tfenne.com>
2005-12-03 22:00:27 +0000

    Cleaned up javadoc and made sure that every file has a license statement at the top of it.

--- stripes/src/net/sourceforge/stripes/tools/SiteStructureTool.java
+++ stripes/src/net/sourceforge/stripes/tools/SiteStructureTool.java
@@ -1,3 +1,18 @@
+/* Copyright (C) 2005 Tim Fennell
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the license with this software. If not,
+ * it can be found online at http://www.fsf.org/licensing/licenses/lgpl.html
+ */
 package net.sourceforge.stripes.tools;
 
 import com.sun.mirror.apt.AnnotationProcessor;
@@ -77,6 +92,9 @@ import java.util.regex.Pattern;
  *   &lt;/exec&gt;
  * &lt;/target&gt;
  *</pre>
+ *
+ * @author Tim Fennell
+ * @since Stripes 1.1.2
  */
 public class SiteStructureTool implements AnnotationProcessor, AnnotationProcessorFactory {
     /** Option name that controls the output format of the annotation processor. */
