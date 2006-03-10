34b8aaa50c4d51b3107731b804928b4a23ad8c0c
Tim Fennell <tfenne@tfenne.com>
2006-03-10 00:33:39 +0000

    Refactored some of the code out of the validation error classes into two new message classes.

--- trunk/stripes/src/net/sourceforge/stripes/action/LocalizableMessage.java
+++ trunk/stripes/src/net/sourceforge/stripes/action/LocalizableMessage.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the license with this software. If not,
  * it can be found online at http://www.fsf.org/licensing/licenses/lgpl.html
  */
-package net.sourceforge.stripes.validation;
+package net.sourceforge.stripes.action;
 
 import net.sourceforge.stripes.controller.StripesFilter;
 
@@ -21,32 +21,14 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
- * <p>Provides a mechanism for creating localizable error messages for presentation to the user.
- * Uses ResourceBundles to provide the localization of the error message.  Messages may contain one
- * or more &quot;replacement parameters &quot;.  Two replacement parameters are provided by default,
- * they are the field name and field value, and are indices 0 and 1 respectively.  To use
- * replacement parameters a message must contain the replacement token {#} where # is the numeric
- * index of the replacement parameter.</p>
+ * A non-error message class that can localize (or at least externalize) the message String
+ * in a resource bundle.  The bundle used is the Stripes error message bundle, which can be
+ * conifgured but by default is called 'StripesResources.properties'.  In all other ways
+ * this class behaves like it's parent {@link SimpleMessage}.
  *
- * <p>For example, to construct an error message with one additional replacement parameter which is
- * the action the user was trying to perform, you might have a properties file entry like:</p>
- *
- * <pre>/action/MyAction.myErrorMessage={1} is not a valid {0} when trying to {2}</pre>
- *
- * <p>At runtime this might get replaced out to result in an error message for the user that looks
- * like &quot;<em>Fixed</em> is not a valid <em>status</em> when trying to create a new
- * <em>bug</em>&quot;.</p>
- *
- * <p>One last point of interest is where the user friendly field name comes from. Firstly an
- * attempt is made to look up the localized name in the applicable resource bundle using the
- * String <em>actionPath.fieldName</em> where actionPath is the action of the form in the JSP
- * (or equally, the path given in the @UrlBinding annotation in the ActionBean class),
- * and fieldName is the name of the field on the form.</p>
- *
- * @see java.text.MessageFormat
- * @see java.util.ResourceBundle
+ * @author Tim Fennell 
  */
-public class LocalizableError extends SimpleError {
+public class LocalizableMessage extends SimpleMessage {
     private String messageKey;
 
     /**
@@ -58,8 +40,8 @@ public class LocalizableError extends SimpleError {
      * @param messageKey a key to lookup a message in the resource bundle
      * @param parameter one or more replacement parameters to insert into the message
      */
-    public LocalizableError(String messageKey, Object... parameter) {
-        super(null, parameter);
+    public LocalizableMessage(String messageKey, Object... parameter) {
+        super((String) null, parameter);
         this.messageKey = messageKey;
     }
 
@@ -76,7 +58,13 @@ public class LocalizableError extends SimpleError {
         return bundle.getString(messageKey);
     }
 
-    /** Generated equals method that compares each field and super.equals(). */
+    /**
+     * Generated equals method which will return true if the other object is of the same
+     * type as this instance, and would produce the same user message.
+     *
+     * @param o an instance of LocalizableMessage or subclass thereof
+     * @return true if the two messages would produce the same user message, false otherwise
+     */
     public boolean equals(Object o) {
         if (this == o) {
             return true;
@@ -88,7 +76,7 @@ public class LocalizableError extends SimpleError {
             return false;
         }
 
-        final LocalizableError that = (LocalizableError) o;
+        final LocalizableMessage that = (LocalizableMessage) o;
 
         if (messageKey != null ? !messageKey.equals(that.messageKey) : that.messageKey != null) {
             return false;
