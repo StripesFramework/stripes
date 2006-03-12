a29eb584bf2166b6e05532402a7ff1ba0d6ce3c8
Tim Fennell <tfenne@tfenne.com>
2006-03-12 19:09:13 +0000

    Modified a custom validation that was including the field value to use the automatically included field value (now that it's fixed).

--- examples/src/net/sourceforge/stripes/examples/bugzooky/web/RegisterActionBean.java
+++ examples/src/net/sourceforge/stripes/examples/bugzooky/web/RegisterActionBean.java
@@ -55,8 +55,7 @@ public class RegisterActionBean extends BugzookyActionBean implements Validatabl
     public void validate(ValidationErrors errors) {
         if ( new PersonManager().getPerson(this.user.getUsername()) != null ) {
             errors.add("user.username",
-                       new LocalizableError("/bugzooky/Register.action.usernameTaken",
-                                            this.user.getUsername()));
+                       new LocalizableError("/bugzooky/Register.action.usernameTaken"));
         }
 
         // We have to check the password for null because it's not supplied
