b65148301bfa39896b92beb7b12ef122a6048f56
Tim Fennell <tfenne@tfenne.com>
2006-04-11 01:28:56 +0000

    Modified the ajax example to use some of the new Stripes features and to use Prototype instead of stripes-ajax.js.

--- trunk/examples/src/net/sourceforge/stripes/examples/ajax/CalculatorActionBean.java
+++ trunk/examples/src/net/sourceforge/stripes/examples/ajax/CalculatorActionBean.java
@@ -3,29 +3,33 @@ package net.sourceforge.stripes.examples.ajax;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.DefaultHandler;
-import net.sourceforge.stripes.action.HandlesEvent;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.StreamingResolution;
 import net.sourceforge.stripes.action.UrlBinding;
 import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidationError;
 import net.sourceforge.stripes.validation.ValidationErrorHandler;
 import net.sourceforge.stripes.validation.ValidationErrors;
-import net.sourceforge.stripes.validation.ValidationError;
 
 import java.io.StringReader;
 import java.util.List;
 
 /**
  * A very simple calculator action that is designed to work with an ajax front end.
+ * Handles 'add' and 'divide' events just like the non-ajax calculator. Each event
+ * calculates the result, and then "streams" it back to the browser. Implements the
+ * ValidationErrorHandler interface to intercept any validation errors, convert them
+ * to an HTML message and stream the back to the browser for display.
+ *
  * @author Tim Fennell
  */
 @UrlBinding("/ajax/Calculator.action")
 public class CalculatorActionBean implements ActionBean, ValidationErrorHandler {
     private ActionBeanContext context;
-    private double numberOne;
-    private double numberTwo;
-    public ActionBeanContext getContext() { return context; }
+    @Validate(required=true) private double numberOne;
+    @Validate(required=true) private double numberTwo;
 
+    public ActionBeanContext getContext() { return context; }
     public void setContext(ActionBeanContext context) { this.context = context; }
 
     /** Converts errors to HTML and streams them back to the browser. */
@@ -34,7 +38,7 @@ public class CalculatorActionBean implements ActionBean, ValidationErrorHandler 
 
         for (List<ValidationError> fieldErrors : errors.values()) {
             for (ValidationError error : fieldErrors) {
-                message.append("<div class=\"error\">");
+                message.append("<div style=\"color: firebrick;\">");
                 message.append(error.getMessage(getContext().getLocale()));
                 message.append("</div>");
             }
@@ -43,23 +47,22 @@ public class CalculatorActionBean implements ActionBean, ValidationErrorHandler 
         return new StreamingResolution("text/html", new StringReader(message.toString()));
     }
 
-    @Validate(required=true)
-    public double getNumberOne() { return numberOne; }
-    public void setNumberOne(double numberOne) { this.numberOne = numberOne; }
-
-    @Validate(required=true)
-    public double getNumberTwo() { return numberTwo; }
-    public void setNumberTwo(double numberTwo) { this.numberTwo = numberTwo; }
-
-    @HandlesEvent("Addition") @DefaultHandler
-    public Resolution addNumbers() {
+    /** Handles the 'add' event, adds the two numbers and returns the result. */
+    @DefaultHandler public Resolution add() {
         String result = String.valueOf(numberOne + numberTwo);
         return new StreamingResolution("text", new StringReader(result));
     }
 
-    @HandlesEvent("Division")
-    public Resolution divideNumbers() {
+    /** Handles the 'divide' event, divides number two by oneand returns the result. */
+    public Resolution divide() {
         String result = String.valueOf(numberOne / numberTwo);
         return new StreamingResolution("text", new StringReader(result));
     }
+
+    // Standard getter and setter methods
+    public double getNumberOne() { return numberOne; }
+    public void setNumberOne(double numberOne) { this.numberOne = numberOne; }
+
+    public double getNumberTwo() { return numberTwo; }
+    public void setNumberTwo(double numberTwo) { this.numberTwo = numberTwo; }
 }
