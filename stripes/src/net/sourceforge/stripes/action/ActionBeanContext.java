bfc4844a4d0d2efeb05575ad696dd18043edc02b
Tim Fennell <tfenne@tfenne.com>
2006-04-27 01:55:19 +0000

    Fixed a javadoc typo.

--- stripes/src/net/sourceforge/stripes/action/ActionBeanContext.java
+++ stripes/src/net/sourceforge/stripes/action/ActionBeanContext.java
@@ -33,7 +33,7 @@ import java.util.ArrayList;
  * to contextual state for their application.  Type safe getters and setter can be added to
  * the subclass and used by the application, thus hiding where the information is actually
  * stored.  This approach is documented in more detail in the Stripes documentation on
- * <a href="http://stripes.mc4j.org/confluence/display/stripes/State+Management>State Management</a>.</p>
+ * <a href="http://stripes.mc4j.org/confluence/display/stripes/State+Management">State Management</a>.</p>
  *
  * @author Tim Fennell
  */
