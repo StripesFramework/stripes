fb5fa909b7aa030e11d59c71b9a22a866b8b9398
Tim Fennell <tfenne@tfenne.com>
2006-05-13 00:29:48 +0000

    Branching the trunk so that work for 1.4 can start to be checked in. The 1.3.x branch will be the source for all 1.3 bugfix releases from here on out.

---
+++ branches/1.3.x/stripes/src/net/sourceforge/stripes/controller/ActionBeanContextFactory.java
@@ -0,0 +1,43 @@
+/* Copyright 2005-2006 Tim Fennell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sourceforge.stripes.controller;
+
+import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.config.ConfigurableComponent;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Interface for classes that can instantiate and supply new instances of the
+ * ActionBeanContext class, or subclasses thereof.
+ *
+ * @author Tim Fennell
+ */
+public interface ActionBeanContextFactory extends ConfigurableComponent {
+
+
+    /**
+     * Creates and returns a new instance of ActionBeanContext or a subclass.
+     *
+     * @param request the current HttpServletRequest
+     * @param response the current HttpServletResponse
+     * @return a new instance of ActionBeanContext
+     * @throws ServletException if the ActionBeanContext class configured cannot be instantiated
+     */
+    ActionBeanContext getContextInstance(HttpServletRequest request,
+                                         HttpServletResponse response) throws ServletException;
+}
---
+++ branches/1.3.x/tests/src/net/sourceforge/stripes/util/CryptoUtilTest.java
@@ -0,0 +1,68 @@
+package net.sourceforge.stripes.util;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import net.sourceforge.stripes.mock.MockHttpServletRequest;
+import net.sourceforge.stripes.mock.MockHttpSession;
+import net.sourceforge.stripes.StripesTestFixture;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Basic tests for the CryptoUtil
+ *
+ * @author Tim Fennell
+ */
+public class CryptoUtilTest {
+
+    /** Manufactures a mock request. */
+    protected HttpServletRequest getRequest() {
+        MockHttpServletRequest request = new MockHttpServletRequest("/foo", "/bar");
+        request.setSession(new MockHttpSession(StripesTestFixture.getServletContext()));
+        return request;
+    }
+
+    @Test(groups="fast")
+    public void basicEncryptionTest() throws Exception {
+        HttpServletRequest request = getRequest();
+
+        String input = "A Basic String to Encrypt";
+        String encrypted = CryptoUtil.encrypt(input, request);
+        String decrypted = CryptoUtil.decrypt(encrypted, request);
+
+        Assert.assertFalse(input.equals(encrypted), "Encrypted string should be different!");
+        Assert.assertTrue(input.equals(decrypted), "Decrypted string should match!");
+    }
+
+    @Test(groups="fast")
+    public void encryptEmptyStringTest() throws Exception {
+        HttpServletRequest request = getRequest();
+
+        String input = "";
+        String encrypted = CryptoUtil.encrypt(input, request);
+        String decrypted = CryptoUtil.decrypt(encrypted, request);
+
+        Assert.assertFalse(input.equals(encrypted), "Encrypted string should be different!");
+        Assert.assertTrue(input.equals(decrypted), "Decrypted string should match!");
+    }
+
+    @Test(groups="fast")
+    public void encryptNullTest() throws Exception {
+        HttpServletRequest request = getRequest();
+
+        String input = null;
+        String encrypted = CryptoUtil.encrypt(input, request);
+
+        Assert.assertNull(encrypted, "Encrypting null should give back null.");
+    }
+
+    @Test(groups="fast")
+    public void decryptNullTest() throws Exception {
+        HttpServletRequest request = getRequest();
+
+        String input = null;
+        String decrypted = CryptoUtil.decrypt(input, request);
+
+        Assert.assertNull(decrypted, "Decrypting null should give back null.");
+    }
+}
