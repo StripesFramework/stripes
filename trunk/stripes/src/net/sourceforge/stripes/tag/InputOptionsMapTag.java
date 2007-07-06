0d4c5d8762649be47e083424da526385220501bb
Aaron Porter <aaron@mongus.com>
2007-07-06 23:57:36 +0000

    Added InputOptionsMapTag by extending InputOptionsCollectionTag and using the results of Map.entrySet() as the collection. label and value parameters are mapped to value and key respectively.
    JIRA issues STS-20 and STS-157

---
+++ trunk/stripes/src/net/sourceforge/stripes/tag/InputOptionsMapTag.java
@@ -0,0 +1,76 @@
+/* Copyright 2007 Aaron Porter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sourceforge.stripes.tag;
+
+import java.util.Map;
+
+import javax.servlet.jsp.JspException;
+
+import net.sourceforge.stripes.tag.InputOptionsCollectionTag;
+
+/**
+ * <p>Extracts the {@link java.util.Set} of {@link java.util.Map.Entry} from the
+ * specified {@link java.util.Map} and uses it as the {@link java.util.Collection}
+ * for the superclass (@link net.sourceforge.stripes.tag.InputOptionsCollectionTag}.</p>
+ * 
+ * <p>The value and label parameters will be set to "key" and "value" respectively
+ * if they are null.</p>
+ *  
+ * @author Aaron Porter
+ *
+ */
+public class InputOptionsMapTag extends InputOptionsCollectionTag
+{
+	private Map<? extends Object,? extends Object> map;
+
+	/**
+	 * @return the {@link java.util.Map} passed in via {@link setMap}
+	 */
+	public Map<? extends Object, ? extends Object> getMap() {
+		return map;
+	}
+
+    /**
+     * <p>This function simply passes the result of {@link java.util.Map.entrySet}
+     * as the collection to be used by the superclass and sets the value and label
+     * variables if they have not already been set.</p>
+     *
+     * @param in a Map
+     */
+	public void setMap(Map<? extends Object, ? extends Object> map)	{
+		this.map = map;
+		
+		setCollection(map.entrySet());
+		
+		if (getValue() == null)
+			setValue("key");
+		
+		if (getLabel() == null)
+			setLabel("value");
+	}
+	
+	/**
+	 * Calls super.doEndTag() and cleans up instance variables so this instance
+	 * may be reused.
+	 */
+    @Override
+    public int doEndTag() throws JspException {
+    	int result = super.doEndTag();
+    	
+    	this.map = null;
+    	
+    	return result;
+    }
+}
