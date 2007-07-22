294eb6101428e5717bc4b817542eb90298437049
Ben Gunter <bgunter@cpons.com>
2007-07-22 05:00:18 +0000

    Now inspects @Validate annotations and automatically includes those properties in the allowed list.
    Changed the API so that one instance is created and cached for each ActionBean class that contains all the information relevant to that class.
    
    Moved the ActionBeanContext security check into BindingPolicyManager.
    
    Fixed a bug in the glob to regex translation for nested properties.
    
    Resolved STS-398: @StrictBinding does not log the properties it blocks.
    
    Also did some additional cleanup.

--- trunk/stripes/src/net/sourceforge/stripes/controller/BindingPolicyManager.java
+++ trunk/stripes/src/net/sourceforge/stripes/controller/BindingPolicyManager.java
@@ -14,6 +14,11 @@
  */
 package net.sourceforge.stripes.controller;
 
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -22,18 +27,21 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.StrictBinding;
 import net.sourceforge.stripes.action.StrictBinding.Policy;
+import net.sourceforge.stripes.exception.StripesRuntimeException;
 import net.sourceforge.stripes.util.Log;
-import net.sourceforge.stripes.util.bean.Node;
-import net.sourceforge.stripes.util.bean.PropertyExpression;
 import net.sourceforge.stripes.util.bean.PropertyExpressionEvaluation;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidateNestedProperties;
 
 /**
- * The policies observed by {@link DefaultActionBeanPropertyBinder} when binding properties to an
- * {@link ActionBean}.
+ * Manages the policies observed by {@link DefaultActionBeanPropertyBinder} when binding properties
+ * to an {@link ActionBean}.
  * 
  * @author Ben Gunter
+ * @see StrictBinding
  */
 @StrictBinding(defaultPolicy = Policy.ALLOW)
 public class BindingPolicyManager {
@@ -46,25 +54,73 @@ public class BindingPolicyManager {
     /** Log */
     private static final Log log = Log.getInstance(BindingPolicyManager.class);
 
-    /** Singleton instance */
-    private static final BindingPolicyManager instance = new BindingPolicyManager();
+    /** Cached instances */
+    private static final Map<Class<?>, BindingPolicyManager> instances = new HashMap<Class<?>, BindingPolicyManager>();
 
-    /** Get the singleton instance of the class */
-    public static BindingPolicyManager getInstance() {
+    /**
+     * Get the policy manager for the given class. Instances are cached and returned on subsequent
+     * calls.
+     * 
+     * @param beanType the class whose policy manager is to be retrieved
+     * @return a policy manager
+     */
+    public static BindingPolicyManager getInstance(Class<?> beanType) {
+        if (instances.containsKey(beanType))
+            return instances.get(beanType);
+
+        BindingPolicyManager instance = new BindingPolicyManager(beanType);
+        instances.put(beanType, instance);
         return instance;
     }
 
-    /** Maps classes to a default policy */
-    private Map<Class<?>, Policy> policy = new HashMap<Class<?>, Policy>();
+    /** The class to which the binding policy applies */
+    private Class<?> beanClass;
 
-    /** Maps classes to a regex that matches the expressions that are allowed to bind */
-    private Map<Class<?>, Pattern> allow = new HashMap<Class<?>, Pattern>();
+    /** The default policy to honor, in case of conflicts */
+    private Policy defaultPolicy;
 
-    /** Maps classes to a regex that matches the expressions that are not allowed to bind */
-    private Map<Class<?>, Pattern> deny = new HashMap<Class<?>, Pattern>();
+    /** The regular expression that allowed properties must match */
+    private Pattern allowPattern;
 
-    /** Does nothing */
-    protected BindingPolicyManager() {
+    /** The regular expression that denied properties must match */
+    private Pattern denyPattern;
+
+    /** The regular expression that matches properties with {@literal @Validate} */
+    private Pattern validatePattern;
+
+    /**
+     * Create a new instance to handle binding security for the given type.
+     * 
+     * @param beanType the type to which the binding policy applies
+     */
+    protected BindingPolicyManager(Class<?> beanClass) {
+        try {
+            log.debug("Creating ", getClass().getName(), " for ", beanClass,
+                    " with default policy ", defaultPolicy);
+            this.beanClass = beanClass;
+
+            // process the annotation
+            StrictBinding annotation = getAnnotation(beanClass);
+            if (annotation != null) {
+                // set default policy
+                this.defaultPolicy = annotation.defaultPolicy();
+
+                // construct the allow pattern
+                this.allowPattern = globToPattern(annotation.allow());
+
+                // construct the deny pattern
+                this.denyPattern = globToPattern(annotation.deny());
+
+                // construct the validated properties pattern
+                this.validatePattern = globToPattern(getValidatedProperties(beanClass));
+            }
+        }
+        catch (Exception e) {
+            log.error(e, "%%% Failure instantiating ", getClass().getName());
+            StripesRuntimeException sre = new StripesRuntimeException(e.getMessage(), e);
+            sre.setStackTrace(e.getStackTrace());
+            throw sre;
+        }
     }
 
     /**
@@ -74,26 +130,24 @@ public class BindingPolicyManager {
      * @return true if binding is allowed; false if not
      */
     public boolean isBindingAllowed(PropertyExpressionEvaluation eval) {
-        PropertyExpression expression = eval.getExpression();
-        Node node = expression.getRootNode();
-        StringBuilder buf = new StringBuilder();
-        do {
-            buf.append(node.getStringValue()).append('.');
-        } while ((node = node.getNext()) != null);
-        if (buf.length() > 0)
-            buf.setLength(buf.length() - 1);
+        // Ensure no-one is trying to bind into the ActionBeanContext!!
+        Type firstNodeType = eval.getRootNode().getValueType();
+        if (firstNodeType instanceof Class
+                && ActionBeanContext.class.isAssignableFrom((Class) firstNodeType)) {
+            return false;
+        }
 
-        Class<?> beanType = eval.getBean().getClass();
-        Pattern denyPattern = getDeniedPattern(beanType);
-        Pattern allowPattern = getAllowedPattern(beanType);
-        boolean deny = denyPattern != null && denyPattern.matcher(buf.toString()).matches();
-        boolean allow = allowPattern != null && allowPattern.matcher(buf.toString()).matches();
+        // check parameter name against access lists
+        String paramName = new ParameterName(eval.getExpression().getSource()).getStrippedName();
+        boolean deny = denyPattern != null && denyPattern.matcher(paramName).matches();
+        boolean allow = (allowPattern != null && allowPattern.matcher(paramName).matches())
+                || (validatePattern != null && validatePattern.matcher(paramName).matches());
 
         /*
          * if path appears on neither or both lists ( i.e. !(allow ^ deny) ) and default policy is
          * to deny access, then fail
          */
-        if (getDefaultPolicy(beanType) == Policy.DENY && !(allow ^ deny))
+        if (defaultPolicy == Policy.DENY && !(allow ^ deny))
             return false;
 
         /*
@@ -109,10 +163,11 @@ public class BindingPolicyManager {
 
     /**
      * Get the {@link StrictBinding} annotation for a class, checking all its superclasses if
-     * necessary.
+     * necessary. If no annotation is found, then one will be returned whose default policy is to
+     * allow binding to all properties.
      * 
      * @param beanType the class to get the {@link StrictBinding} annotation for
-     * @return
+     * @return An annotation. This method never returns null.
      */
     protected StrictBinding getAnnotation(Class<?> beanType) {
         StrictBinding annotation;
@@ -126,54 +181,115 @@ public class BindingPolicyManager {
     }
 
     /**
-     * Get the default policy for the given class.
+     * Get all the properties and nested properties of the given class to which a {@link Validate}
+     * annotation is applied. The annotation may be applied to the property's read method, write
+     * method, or field declaration. If a property has a {@link ValidateNestedProperties}
+     * annotation, then the nested properties named in its {@link Validate} annotations will be
+     * included as well. However, the {@link ValidateNestedProperties} annotation alone is not
+     * sufficient to include a property; it must have its own {@link Validate} annotation.
+     * 
+     * @param beanClass a class
+     * @return The validated properties. If no properties are annotated then null.
+     */
+    protected String[] getValidatedProperties(Class<?> beanClass) {
+        // for the sake of pretty code, i declare these first
+        Method method;
+        Validate simple;
+        ValidateNestedProperties nested;
+        List<Validate> simples = new ArrayList<Validate>();
+        List<ValidateNestedProperties> nesteds = new ArrayList<ValidateNestedProperties>();
+        List<String> properties = new ArrayList<String>();
+        try {
+            PropertyDescriptor[] pds = Introspector.getBeanInfo(beanClass).getPropertyDescriptors();
+            for (PropertyDescriptor pd : pds) {
+                String propertyName = pd.getName();
+                simples.clear();
+                nesteds.clear();
+
+                // check getter method
+                method = pd.getReadMethod();
+                if (method != null) {
+                    simple = method.getAnnotation(Validate.class);
+                    nested = method.getAnnotation(ValidateNestedProperties.class);
+                    if (simple != null)
+                        simples.add(simple);
+                    if (nested != null)
+                        nesteds.add(nested);
+                }
+
+                // check setter method
+                method = pd.getWriteMethod();
+                if (method != null) {
+                    simple = method.getAnnotation(Validate.class);
+                    nested = method.getAnnotation(ValidateNestedProperties.class);
+                    if (simple != null)
+                        simples.add(simple);
+                    if (nested != null)
+                        nesteds.add(nested);
+                }
+
+                // check the field (possibly declared in a superclass) with the same
+                // name as the property for annotations
+                Field field = null;
+                for (Class<?> c = beanClass; c != null && field == null; c = c.getSuperclass()) {
+                    try {
+                        field = c.getDeclaredField(propertyName);
+                    }
+                    catch (NoSuchFieldException e) {
+                    }
+                }
+                if (field != null) {
+                    simple = field.getAnnotation(Validate.class);
+                    nested = field.getAnnotation(ValidateNestedProperties.class);
+                    if (simple != null)
+                        simples.add(simple);
+                    if (nested != null)
+                        nesteds.add(nested);
+                }
+
+                // add to allow list if @Validate present
+                if (!simples.isEmpty())
+                    properties.add(propertyName);
+
+                // add all sub-properties referenced in @ValidateNestedProperties
+                for (ValidateNestedProperties vnp : nesteds) {
+                    Validate[] validates = vnp.value();
+                    if (validates != null) {
+                        for (Validate validate : validates) {
+                            if (validate.field() != null) {
+                                properties.add(propertyName + '.' + validate.field());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e) {
+            log.error(e, "%%% Failure checking @Validate annotations ", getClass().getName());
+            StripesRuntimeException sre = new StripesRuntimeException(e.getMessage(), e);
+            sre.setStackTrace(e.getStackTrace());
+            throw sre;
+        }
+        return properties.size() == 0 ? null : properties.toArray(new String[properties.size()]);
+    }
+
+    /**
+     * Get the bean class.
+     * 
+     * @return the bean class
+     */
+    public Class<?> getBeanClass() {
+        return beanClass;
+    }
+
+    /**
+     * Get the default policy.
      * 
      * @param beanType the class whose policy is to be looked up
      * @return the policy
      */
-    protected Policy getDefaultPolicy(Class<?> beanType) {
-        if (policy.containsKey(beanType))
-            return policy.get(beanType);
-
-        Policy defaultPolicy = getAnnotation(beanType).defaultPolicy();
-        policy.put(beanType, defaultPolicy);
+    public Policy getDefaultPolicy() {
         return defaultPolicy;
-    }
-
-    /**
-     * Get the {@link Pattern} against which property names that are allowed to bind will be
-     * matched.
-     * 
-     * @param beanType {@link ActionBean} type
-     * @return A pattern against which property names will be matched. This method must not return
-     *         null.
-     */
-    protected Pattern getAllowedPattern(Class<?> beanType) {
-        if (allow.containsKey(beanType))
-            return allow.get(beanType);
-
-        StrictBinding annotation = getAnnotation(beanType);
-        Pattern pattern = globToPattern(annotation.allow());
-        allow.put(beanType, pattern);
-        return pattern;
-    }
-
-    /**
-     * Get the {@link Pattern} against which property names that are not allowed to bind will be
-     * matched.
-     * 
-     * @param beanType {@link ActionBean} type
-     * @return A pattern against which property names will be matched. This method must not return
-     *         null.
-     */
-    protected Pattern getDeniedPattern(Class<?> beanType) {
-        if (deny.containsKey(beanType))
-            return deny.get(beanType);
-
-        StrictBinding annotation = getAnnotation(beanType);
-        Pattern pattern = globToPattern(annotation.deny());
-        deny.put(beanType, pattern);
-        return pattern;
     }
 
     /**
@@ -204,13 +320,9 @@ public class BindingPolicyManager {
             for (int i = 0; i < properties.length; i++) {
                 String property = properties[i];
                 if ("*".equals(property)) {
-                    if (i > 0)
-                        buf.append("\\.");
                     buf.append(PROPERTY_REGEX);
                 }
                 else if ("**".equals(property)) {
-                    if (i > 0)
-                        buf.append("\\.");
                     buf.append(PROPERTY_REGEX).append("(\\.").append(PROPERTY_REGEX).append(")*");
                 }
                 else if (property.length() > 0) {
