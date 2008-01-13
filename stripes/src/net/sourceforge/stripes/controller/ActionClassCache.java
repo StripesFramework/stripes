0eae9a44646fc922d8d5d0c0c0be38e8da18c9f1
Ben Gunter <bgunter@cpons.com>
2008-01-13 05:31:03 +0000

    Deprecated ActionClassCache and removed the reference to it from AnnotatedClassActionResolver.init(...)

--- stripes/src/net/sourceforge/stripes/controller/ActionClassCache.java
+++ stripes/src/net/sourceforge/stripes/controller/ActionClassCache.java
@@ -24,6 +24,7 @@ import java.util.Set;
  *
  * @author Tim Fennell
  */
+@Deprecated
 public class ActionClassCache {
     /** The static singleton instance of this class. */
     private static ActionClassCache cache;
