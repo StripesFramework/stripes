0e075a96482c45be871d71468700ad59654ce8b9
Tim Fennell <tfenne@tfenne.com>
2008-01-19 02:22:35 +0000

    Big hairy fix for STS-427.  Not convinced it fixes all possible cases, but it fixes all cases for which we have test cases!  Ha!

--- trunk/tests/src/net/sourceforge/stripes/test/TestGenericBean.java
+++ trunk/tests/src/net/sourceforge/stripes/test/TestGenericBean.java
@@ -1,5 +1,7 @@
 package net.sourceforge.stripes.test;
 
+import java.lang.reflect.Method;
+
 /**
  * A JavaBean that is a generic type.
  *
@@ -13,24 +15,21 @@ package net.sourceforge.stripes.test;
  *     class Class2<B,A> extends Class1<B,A> {}
  *     class Class3<X,Y> extends Class2<X,Y> {}
  *     public class TestGenericBean<A,B> extends Class3<A,B> {}
- * Stripes will fail to bind the properties of Class1 correctly.  See STS-XXX
+ * Stripes will fail to bind the properties of Class1 correctly.  See STS-427
  */
-public class TestGenericBean<A,B> {
-    private A genericA;
-    private B genericB;
-
-    public A getGenericA() {
-        return genericA;
-    }
-    public void setGenericA(A genericA) {
-        this.genericA = genericA;
-    }
-
-    public B getGenericB() {
-        return genericB;
+public class TestGenericBean {
+    public static class Class1<A,B> {
+        private A genericA;
+        private B genericB;
+        public A getGenericA() { return genericA; }
+        public void setGenericA(A genericA) { this.genericA = genericA; }
+        public B getGenericB() { return genericB; }
+        public void setGenericB(B genericB) { this.genericB = genericB; }
     }
 
-    public void setGenericB(B genericB) {
-        this.genericB = genericB;
-    }
+    public static class Class2<B,A> extends Class1<B,A> {}
+    public static class Class3<A,B> extends Class2<A,B> {}
+
+    public static class GenericBean<G,H> extends Class3<G,H> {}
 }
+
