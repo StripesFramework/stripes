9b81c03bf215baf28c8013ce7c1bd6282fea030f
Aaron Porter <aaron@mongus.com>
2008-01-24 20:37:12 +0000

    STS-492: auto-discovery code moved into BootstrapPropertyResolver

--- trunk/stripes/src/net/sourceforge/stripes/config/BootstrapPropertyResolver.java
+++ trunk/stripes/src/net/sourceforge/stripes/config/BootstrapPropertyResolver.java
@@ -14,7 +14,17 @@
  */
 package net.sourceforge.stripes.config;
 
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+
 import javax.servlet.FilterConfig;
+
+import net.sourceforge.stripes.exception.StripesRuntimeException;
+import net.sourceforge.stripes.util.Log;
+import net.sourceforge.stripes.util.ReflectUtil;
+import net.sourceforge.stripes.util.ResolverUtil;
+import net.sourceforge.stripes.util.StringUtil;
 
 /**
  * <p>Resolves configuration properties that are used to bootstrap the system.  Essentially this boils
@@ -33,7 +43,12 @@ import javax.servlet.FilterConfig;
  * @author Tim Fennell
  */
 public class BootstrapPropertyResolver {
+    private static final Log log = Log.getInstance(BootstrapPropertyResolver.class);
+    
     private FilterConfig filterConfig;
+
+    /** The Configuration Key for looking up the comma separated list of extension packages. */
+    public static final String EXTENSION_LIST = "Extension.Packages";
 
     /** Constructs a new BootstrapPropertyResolver with the given ServletConfig. */
     public BootstrapPropertyResolver(FilterConfig filterConfig) {
@@ -69,5 +84,125 @@ public class BootstrapPropertyResolver {
         }
 
         return value;
+    }
+    
+    /**
+     * Attempts to find a class the user has specified in web.xml or by auto-discovery in packages listed in web.xml under Extension.Packages. Classes specified in web.xml take precedence.   
+     * 
+     * @param paramName the parameter to look for in web.xml
+     * @param targetType the type that we're looking for
+     * @return the Class that was found
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Class<? extends T> getClassProperty(String paramName, Class<T> targetType)
+    {
+        Class<? extends T> clazz = null;
+        
+        String className = getProperty(paramName);
+        
+        if (className != null)
+        {
+            // web.xml takes precedence
+            try {
+                clazz = (Class<? extends T>) ReflectUtil.findClass(className);
+                log.info("Class implementing/extending ", targetType.getSimpleName(), " found in web.xml: ", className);
+            }
+            catch (ClassNotFoundException e) {
+                log.error("Couldn't find class specified in web.xml under param ", paramName, ": ", className);
+            }
+        }
+        else
+        {
+            // we didn't find it in web.xml so now we check any extension packages
+            ResolverUtil<T> resolver = new ResolverUtil<T>();
+            String[] packages = StringUtil.standardSplit(getProperty(EXTENSION_LIST));
+            resolver.findImplementations(targetType, packages);
+            Set<Class<? extends T>> classes = resolver.getClasses();
+            if (classes.size() == 1) {
+                clazz = classes.iterator().next();
+                className = clazz.getName();
+                log.info("Class implementing/extending  ", targetType.getSimpleName(), " found via auto-discovery: ", className);
+            }
+            else if (classes.size() > 1) {
+                throw new StripesRuntimeException(StringUtil.combineParts("Found too many classes implementing/extending ",
+                                        targetType.getSimpleName(),
+                                        ": ",
+                                        classes));
+            }
+        }
+
+        return clazz;
+    }
+    
+    /**
+     * Attempts to find all classes the user has specified in web.xml.
+     *
+     * @param paramName the parameter to look for in web.xml
+     * @return a List of classes found
+     */
+    @SuppressWarnings("unchecked")
+    public List<Class> getClassPropertyList(String paramName)
+    {
+        List<Class> classes = new Vector<Class>();
+        
+        String classList = getProperty(paramName);
+        
+        if (classList != null) {
+            String[] classNames = StringUtil.standardSplit(classList);
+            for (String className : classNames) {
+                className = className.trim();
+                try {
+                    classes.add(ReflectUtil.findClass(className));
+                }
+                catch (ClassNotFoundException e) {
+                    throw new StripesRuntimeException("Could not find configured Interceptor ["
+                            + className + "]. The " + "property '" + paramName
+                            + "' contained [" + classList
+                            + "]. This value must contain fully qualified class names separated "
+                            + "by commas.");
+                }
+            }
+        }
+
+        return classes;
+    }
+    
+    /**
+     * Attempts to find classes by auto-discovery in packages listed in web.xml under Extension.Packages.   
+     * 
+     * @param targetType the type that we're looking for
+     * @return a List of classes found
+     */
+    public <T> List<Class<? extends T>> getClassPropertyList(Class<T> targetType)
+    {
+        List<Class<? extends T>> classes = new Vector<Class<? extends T>>();
+
+        ResolverUtil<T> resolver = new ResolverUtil<T>();
+        String[] packages = StringUtil.standardSplit(getProperty(EXTENSION_LIST));
+        resolver.findImplementations(targetType, packages);
+        classes.addAll(resolver.getClasses());
+
+        return classes;
+    }
+
+    /**
+     * Attempts to find all matching classes the user has specified in web.xml or by auto-discovery in packages listed in web.xml under Extension.Packages.   
+     * 
+     * @param paramName the parameter to look for in web.xml
+     * @param targetType the type that we're looking for
+     * @return the Class that was found
+     */
+    @SuppressWarnings("unchecked")
+    public <T> List<Class<? extends T>> getClassPropertyList(String paramName, Class<T> targetType)
+    {
+        List<Class<? extends T>> classes = new Vector<Class<? extends T>>();
+        
+        for (Class<?> clazz : getClassPropertyList(paramName))
+            // can't use addAll :(
+            classes.add((Class<? extends T>) clazz);
+        
+        classes.addAll(getClassPropertyList(targetType));
+
+        return classes;
     }
 }
