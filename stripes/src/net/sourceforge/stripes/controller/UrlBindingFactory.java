3db4f9df4aaf13f1ec9e61b066b6b814ff857fff
Ben Gunter <bgunter@cpons.com>
2008-02-05 16:58:08 +0000

    Wrong file header.

--- stripes/src/net/sourceforge/stripes/controller/UrlBindingFactory.java
+++ stripes/src/net/sourceforge/stripes/controller/UrlBindingFactory.java
@@ -1,10 +1,16 @@
-/**
- * $Id$
- * $Name$
+/* Copyright 2007 Ben Gunter
  *
- * Created on Jun 8, 2007 at 10:18:03 AM by Ben Gunter.
- * 
- * Copyright 2007 Cpons.com, Inc. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.sourceforge.stripes.controller;
 
