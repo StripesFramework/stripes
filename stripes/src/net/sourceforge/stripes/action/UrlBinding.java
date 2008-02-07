dd05b296ccc940074f7ba1fd3ad76fa1d04bc8c0
Ben Gunter <bgunter@cpons.com>
2008-02-07 04:20:53 +0000

    Updated Javadoc

--- stripes/src/net/sourceforge/stripes/action/UrlBinding.java
+++ stripes/src/net/sourceforge/stripes/action/UrlBinding.java
@@ -22,29 +22,37 @@ import java.lang.annotation.ElementType;
 
 /**
  * <p>
- * Annotation used to bind ActionBean classes to a specific path within the web application.
- * The AnnotatedClassActionResolver will examine the URL submitted and extract the section
- * that is relative to the web-app root.  That will be compared with the URL specified in
- * the UrlBinding annotation, to find the ActionBean that should process the chosen request.
+ * Annotation used to bind ActionBean classes to a specific path within the web application. The
+ * AnnotatedClassActionResolver will examine the URL submitted and extract the section that is
+ * relative to the web-app root. That will be compared with the URL specified in the UrlBinding
+ * annotation, to find the ActionBean that should process the chosen request.
  * </p>
  * <p>
- * Stripes supports "Clean URLs" through the {@link UrlBinding} annotation. Parameters may
- * be embedded in the URL by placing the parameter name inside braces ({}). For example,
- * <code>@UrlBinding("/foo/{bar}/{baz}")</code> maps the action to "/foo" and indicates
- * that the "bar" and "baz" parameters may be embedded in the URL. In this case, the URL
- * /foo/abc/123 would invoke the action with bar set to "abc" and baz set to "123". The
- * literal strings between parameters can be any string.  
+ * Stripes supports "Clean URLs" through the {@link UrlBinding} annotation. Parameters may be
+ * embedded in the URL by placing the parameter name inside braces ({}). For example,
+ * {@code @UrlBinding("/foo/{bar}/{baz}")} maps the action to "/foo" and indicates that the "bar"
+ * and "baz" parameters may be embedded in the URL. In this case, the URL /foo/abc/123 would invoke
+ * the action with bar set to "abc" and baz set to "123". The literal strings between parameters can
+ * be any string.
  * </p>
  * <p>
- * The special parameter name $event may be used to embed the event name in a clean URL.
- * For example, given <code>@UrlBinding("/foo/{$event}")</code> the "bar" event could be
- * invoked with the URL /foo/bar. 
+ * The special parameter name $event may be used to embed the event name in a clean URL. For
+ * example, given {@code @UrlBinding("/foo/{$event}")} the "bar" event could be invoked with the
+ * URL /foo/bar.
  * </p>
  * <p>
- * Any number of parameters and/or literals may be excluded from the end of a request URL.
- * The missing parameters will simply be null when the request is dispatched.
+ * Clean URL parameters can be assigned default values using the {@code =} operator. For example,
+ * {@code @UrlBinding("/foo/{bar=abc}/{baz=123}")}. If a parameter with a default value is missing
+ * from a request URL, it will still be made available as a request parameter with the default
+ * value. Default values are automatically embedded when building URLs with the Stripes JSP tags.
+ * The default value for $event can be automatically determined from the {@link DefaultHandler}
+ * annotation or it can be explicitly set like any other parameter.
  * </p>
- *
+ * <p>
+ * Clean URLs support both prefix mapping ({@code /action/foo/{bar}}) and extension mapping ({@code /foo/{bar}.action}).
+ * Any number of parameters and/or literals may be omitted from the end of a request URL.
+ * </p>
+ * 
  * @author Tim Fennell
  */
 @Retention(RetentionPolicy.RUNTIME)
