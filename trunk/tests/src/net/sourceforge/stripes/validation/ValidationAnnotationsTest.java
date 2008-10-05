5463c2f6b7fe107e79f1e6544f04299723fcb334
Frederic Daoud <xf2697@fastmail.fm>
2008-10-05 02:37:10 +0000

    Fix for STS-610 with unit tests

--- trunk/tests/src/net/sourceforge/stripes/validation/ValidationAnnotationsTest.java
+++ trunk/tests/src/net/sourceforge/stripes/validation/ValidationAnnotationsTest.java
@@ -4,6 +4,9 @@ import net.sourceforge.stripes.StripesTestFixture;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.controller.StripesFilter;
+import net.sourceforge.stripes.extensions.MyIntegerTypeConverter;
+import net.sourceforge.stripes.extensions.MyStringTypeConverter;
 import net.sourceforge.stripes.mock.MockRoundtrip;
 
 import org.testng.Assert;
@@ -37,7 +40,7 @@ public class ValidationAnnotationsTest implements ActionBean {
         MockRoundtrip trip = new MockRoundtrip(StripesTestFixture.getServletContext(), getClass());
         trip.execute("validateRequiredAndIgnored");
         ActionBean actionBean = trip.getActionBean(getClass());
-        Assert.assertEquals(0, actionBean.getContext().getValidationErrors().size());
+        Assert.assertEquals(actionBean.getContext().getValidationErrors().size(), 0);
     }
 
     @Validate(required=true, on="validatePublicField")
@@ -55,6 +58,52 @@ public class ValidationAnnotationsTest implements ActionBean {
         MockRoundtrip trip = new MockRoundtrip(StripesTestFixture.getServletContext(), getClass());
         trip.execute("validatePublicField");
         ActionBean actionBean = trip.getActionBean(getClass());
-        Assert.assertEquals(1, actionBean.getContext().getValidationErrors().size());
+        Assert.assertEquals(actionBean.getContext().getValidationErrors().size(), 1);
+    }
+
+    public Integer shouldBeDoubled;
+    @Validate(converter=IntegerTypeConverter.class) public Integer shouldNotBeDoubled;
+
+    public String shouldBeUpperCased;
+    @Validate(converter=StringTypeConverter.class) public String shouldNotBeUpperCased;
+
+    public Resolution validateTypeConverters() { return null; }
+
+    /**
+     * Tests the use of an auto-loaded type converter versus a type converter explicitly configured
+     * via {@code @Validate(converter)}, where the auto-loaded type converter extends the stock
+     * type converter.
+     *
+     * @see http://www.stripesframework.org/jira/browse/STS-610
+     */
+    @Test(groups="extensions")
+    public void testValidateTypeConverterExtendsStock() throws Exception {
+        MockRoundtrip trip = new MockRoundtrip(StripesTestFixture.getServletContext(), getClass());
+        StripesFilter.getConfiguration().getTypeConverterFactory().add(Integer.class, MyIntegerTypeConverter.class);
+        trip.addParameter("shouldBeDoubled", "42");
+        trip.addParameter("shouldNotBeDoubled", "42");
+        trip.execute("validateTypeConverters");
+        ValidationAnnotationsTest actionBean = trip.getActionBean(getClass());
+        Assert.assertEquals(actionBean.shouldBeDoubled, new Integer(84));
+        Assert.assertEquals(actionBean.shouldNotBeDoubled, new Integer(42));
+    }
+
+    /**
+     * Tests the use of an auto-loaded type converter versus a type converter explicitly configured
+     * via {@code @Validate(converter)}, where the auto-loaded type converter does not extend the
+     * stock type converter.
+     *
+     * @see http://www.stripesframework.org/jira/browse/STS-610
+     */
+    @Test(groups="extensions")
+    public void testValidateTypeConverterDoesNotExtendStock() throws Exception {
+        MockRoundtrip trip = new MockRoundtrip(StripesTestFixture.getServletContext(), getClass());
+        StripesFilter.getConfiguration().getTypeConverterFactory().add(String.class, MyStringTypeConverter.class);
+        trip.addParameter("shouldBeUpperCased", "test");
+        trip.addParameter("shouldNotBeUpperCased", "test");
+        trip.execute("validateTypeConverters");
+        ValidationAnnotationsTest actionBean = trip.getActionBean(getClass());
+        Assert.assertEquals(actionBean.shouldBeUpperCased, "TEST");
+        Assert.assertEquals(actionBean.shouldNotBeUpperCased, "test");
     }
 }
