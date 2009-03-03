7126ec49860986cf7466888cf86eb73675893c68
Ben Gunter <bgunter@cpons.com>
2009-03-03 01:35:39 +0000

    Rolled back the following commit because we no longer think it is appropriate for release 1.5.1. Look for this change in 1.6
    ========================================================================
    r1069 | bengunter | 2009-02-27 11:12:16 -0600 (Fri, 27 Feb 2009) | 4 lines
    
    Added @Deprecated annotation to these classes and updated the Javadoc
    @deprecated comment to say deprecated as of 1.5.1 and replaced by
    SpringInjectionPostProcessor.

--- branches/1.5.x/stripes/src/net/sourceforge/stripes/integration/spring/SpringInterceptorSupport.java
+++ branches/1.5.x/stripes/src/net/sourceforge/stripes/integration/spring/SpringInterceptorSupport.java
@@ -31,11 +31,8 @@ import javax.servlet.ServletContext;
  * only once when the interceptor is first created and initialized.</p>
  *
  * @author Tim Fennell
- * @since Stripes 1.4
- * @deprecated As of Stripes 1.5.1, replaced by {@link SpringInjectionPostProcessor} which already
- *             injects Spring dependencies into Interceptors.
+ * @since Stripes 1.4 
  */
-@Deprecated
 public abstract class SpringInterceptorSupport implements Interceptor, ConfigurableComponent {
 
     /**
