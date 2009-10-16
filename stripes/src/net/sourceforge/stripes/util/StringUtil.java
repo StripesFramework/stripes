a4ef97b48c4bf4fb6227d8aca922d3c15a6f3198
Ben Gunter <bgunter@cpons.com>
2009-10-16 20:18:51 +0000

    Fixed STS-680: Anchors are not URL encoded when generated with s:link anchor attribute. Anchors are now encoded as a URI fragment according to my interpretation of RFC 3986.

--- stripes/src/net/sourceforge/stripes/util/StringUtil.java
+++ stripes/src/net/sourceforge/stripes/util/StringUtil.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.sourceforge.stripes.exception.StripesRuntimeException;
@@ -20,6 +21,14 @@ public class StringUtil {
      * separated by any whitespace (including new lines) and/or a comma.
      */
     private static final Pattern STANDARD_SPLIT = Pattern.compile("[\\s,]+");
+
+    /**
+     * A regular expression that matches characters that are not explicitly allowed in the fragment
+     * part of a URI according to RFC 3986. This does not include the percent sign (%), which is
+     * actually allowed but only as an escape character for percent-encoded characters.
+     */
+    private static final Pattern URI_FRAGMENT_DISALLOWED_CHARACTERS = Pattern
+            .compile("[^\\p{Alnum}._~!$&'()*+,;=:@/?-]");
 
     /**
      * Splits apart the input String on any whitespace and/or commas. Leading and trailing
@@ -83,5 +92,39 @@ public class StringUtil {
         catch (UnsupportedEncodingException e) {
             throw new StripesRuntimeException("Unsupported encoding?  UTF-8?  That's unpossible.");
         }
+    }
+
+    /**
+     * Encode a URI fragment as required by RFC 3986. The fragment is allowed to contain a different
+     * set of characters than other parts of the URI, and characters that are allowed in the
+     * fragment must not be encoded.
+     * 
+     * @param value The string to encode
+     * @return The encoded string
+     */
+    public static String uriFragmentEncode(String value) {
+        Matcher matcher = URI_FRAGMENT_DISALLOWED_CHARACTERS.matcher(value);
+
+        StringBuilder buf = null;
+        int end = 0;
+        while (matcher.find()) {
+            if (buf == null) {
+                buf = new StringBuilder(value.length() * 2);
+            }
+
+            buf.append(value.substring(end, matcher.start())).append(
+                    String.format("%%%02X", (int) matcher.group().charAt(0)));
+            end = matcher.end();
+        }
+
+        // No match, return input unchanged
+        if (buf == null)
+            return value;
+
+        // Append tail
+        if (end < value.length())
+            buf.append(value.substring(end));
+
+        return buf.toString();
     }
 }
