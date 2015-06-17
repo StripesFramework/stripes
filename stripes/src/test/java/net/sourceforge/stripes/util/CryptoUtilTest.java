b684cb302bd0e28f4e6dd2d39df1f7f9b0cc738e
Peter Magnusson <PeterMagnusson@users.noreply.github.com>
2015-06-17 19:36:44 +0200

    STS-934 change CryptoUtil
    https://stripesframework.atlassian.net/browse/STS-934
    Encrypt-then-Mac (EtM).
    Switch from ECB to CBC.

--- stripes/src/test/java/net/sourceforge/stripes/util/CryptoUtilTest.java
+++ stripes/src/test/java/net/sourceforge/stripes/util/CryptoUtilTest.java
@@ -1,5 +1,7 @@
 package net.sourceforge.stripes.util;
 
+import java.security.SecureRandom;
+
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
@@ -21,6 +23,15 @@ public class CryptoUtilTest {
 
         Assert.assertFalse(input.equals(encrypted), "Encrypted string should be different!");
         Assert.assertTrue(input.equals(decrypted), "Decrypted string should match!");
+        
+        input = "";
+        for(int i = 0; i < 100; i++) {
+            encrypted = CryptoUtil.encrypt(input);
+            decrypted = CryptoUtil.decrypt(encrypted);
+
+            Assert.assertTrue(input.equals(decrypted), "Decrypted string should match!");
+            input += "x";
+        }
     }
 
     @Test(groups="fast")
@@ -62,7 +73,7 @@ public class CryptoUtilTest {
         SecretKey oldKey = CryptoUtil.getSecretKey(); // cache the old key
 
         try {
-            KeyGenerator gen = KeyGenerator.getInstance("AES");
+            KeyGenerator gen = KeyGenerator.getInstance(CryptoUtil.ALGORITHM);
             SecretKey key = gen.generateKey();
             CryptoUtil.setSecretKey(key);
             String input = "A string to be encrypted with a different algorigthm and key!";
@@ -75,5 +86,38 @@ public class CryptoUtilTest {
             CryptoUtil.setSecretKey(oldKey);
         }
     }
+
+	/** 
+	 * This test is disabled because it is very very slow.
+	 * It will launch a modified ciphertext attack, which should always be rejected by hmac verification.
+	 */
+    @Test(groups="slow", enabled=false)
+	public void failOnWeakHash() throws Exception {
+		String input = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		String encrypted = CryptoUtil.encrypt(input);
+		SecureRandom rnd = new SecureRandom();
+		byte[] random = new byte[64];
+		int options = Base64.URL_SAFE | Base64.DONT_BREAK_LINES;
+		byte[] choosen = Base64.decode(encrypted, options);
+		for (int attempts = 0; attempts < Integer.MAX_VALUE; attempts++) {
+			rnd.nextBytes(random);
+			System.arraycopy(random, 0, choosen, CryptoUtil.CIPHER_BLOCK_LENGTH, CryptoUtil.CIPHER_BLOCK_LENGTH);
+			String choosenciphertext = Base64.encodeBytes(choosen, options);
+			String broken = CryptoUtil.decrypt(choosenciphertext);
+			Assert.assertNull(broken, "hash failed: " + choosenciphertext
+					+ " derived from " + encrypted);
+		}
+	}
+    
+	@Test(groups = "fast")
+	public void failOnECB() throws Exception {
+		String input1 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		String encrypted1 = CryptoUtil.encrypt(input1);
+		String encrypted2 = CryptoUtil.encrypt(input1);
+		for (int i = 0; i < encrypted1.length() - 4; i++)
+			Assert.assertFalse(
+					encrypted2.contains(encrypted1.substring(i, i + 4)),
+					"Predictable ECB detected: " + encrypted1 + " " + encrypted2);
+	}
 
 }
